7f949c3c514219bea1abb50889bd20d4720aede7
ben-pfeffer <bpfffr@gmail.com>
2025-05-14 16:02:50 -0400

    add get_da_hourly_prices function

---
+++ R/get_da_hourly_prices.R
@@ -0,0 +1,44 @@
+#' Easily gather hourly DA prices
+#' @param iso Name of dataset to download
+#' @param start_time Start time. Default is 5 days ago.
+#' @param end_time End time. Default is today's date.
+#' @param limit Number of requests to return. Default is 100.
+#' @param timezone Default is market, which returns times in the market's local time
+#' @param location filter term for the data's location column
+#' @param location_type filter term for the data's location_type column
+#' @returns dataset of hourly DA Prices for the specified ISO and date range
+#' @export
+#' @seealso [construct_query_url(), get_api_request()]
+#'
+
+get_da_hourly_prices <- function(iso,
+                                 start_time = Sys.Date() - 5,
+                                 end_time = Sys.Date(),
+                                 limit = 100,
+                                 timezone = 'market',
+                                 location = '',
+                                 location_type = '') {
+
+  allowed_isos <- c('ercot', 'caiso', 'isone', 'miso', 'nyiso', 'pjm', 'spp')
+  if(!iso %in% allowed_isos) {
+    stop(paste('Invalid ISO entered in get_da_hourly_prices. Must be one of', list(allowed_isos)))
+  }
+
+  wh_dataset <- dplyr::case_when(iso == 'ercot' ~ 'ercot_spp_day_ahead_hourly',
+                                 iso == 'caiso' ~ 'caiso_lmp_day_ahead_hourly',
+                                 iso == 'isone' ~ 'isone_lmp_real_time_hourly_final',
+                                 iso == 'miso' ~ 'miso_lmp_day_ahead_hourly',
+                                 iso == 'nyiso' ~ 'nyiso_lmp_day_ahead_hourly',
+                                 iso == 'pjm' ~ 'pjm_lmp_day_ahead_hourly',
+                                 iso == 'spp' ~ 'spp_lmp_day_ahead_hourly')
+
+  req_url <- construct_query_url(wh_dataset,
+                                 start_time,
+                                 end_time,
+                                 limit,
+                                 timezone,
+                                 location,
+                                 location_type)
+
+  data <- get_api_request(req_url)
+}
