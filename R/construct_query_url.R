7295d9aa736c6a69064db108678f8ea3bb43bae5
ben-pfeffer <bpfffr@gmail.com>
2025-05-14 10:41:47 -0400

    rename to rgridstatus2; add query url parameter options; add option to return raw json

--- R/construct_query_url.R
+++ R/construct_query_url.R
@@ -2,6 +2,10 @@
 #' @param wh_dataset Name of dataset to download
 #' @param start_time Start time. Default is 5 days ago.
 #' @param end_time End time. Default is today's date.
+#' @param limit Number of requests to return. Default is 100.
+#' @param timezone Default is market, which returns times in the market's local time
+#' @param location filter term for the data's location column
+#' @param location_type filter term for the data's location_type column
 #' @returns req_url URL for API request with specified parameters
 #' @export
 #' @seealso [get_available_datasets()]
@@ -10,18 +14,41 @@
 #'
 #'
 
-construct_query_url <- function(wh_dataset = "caiso_fuel_mix",
+construct_query_url <- function(wh_dataset,
                                 start_time = Sys.Date() - 5,
-                                end_time = Sys.Date()) {
+                                end_time = Sys.Date(),
+                                limit = 100,
+                                timezone = 'market',
+                                location = '',
+                                location_type = '') {
 
   # base url for the Gridstatusio API
   base_url <- "https://api.gridstatus.io/v1/"
 
-  # construct full request url
+  # construct partial request url
   req_url <- paste0(
     base_url, "datasets/", wh_dataset, "/query?",
     "download=true",
     "&start_time=", start_time,
-    "&end_time=", end_time
+    "&end_time=", end_time,
+    '&timezone=', timezone,
+    '&limit=', limit
   )
+
+  # add optional filter parameters
+  if(!location == '') {
+    location_clean <- stringr::str_replace_all(location, ' ', '%20')
+
+    req_url <- paste0(req_url,
+                      '&location=', location_clean)
+  }
+
+  if(!location_type == '') {
+    location_type_clean <- stringr::str_replace_all(location_type, ' ', '%20')
+
+    req_url <- paste0(req_url,
+                      '&location_type=', location_type_clean)
+  }
+
+  return(req_url)
 }
--- R/get_api_request.R
+++ R/get_api_request.R
@@ -1,10 +1,12 @@
 #' Make API GET request and return data
 #' @param query_url URL for the API query
 #' @param api_key API key; Defaults is key saved in .renviron file as "GRIDSTATUS_API_KEY"
+#' @param parse_request boolean - TRUE returns data; false returns raw JSON. Default is TRUE
 #' @returns data from the API response
 #' @export
 
-get_api_request <- function(query_url, api_key = Sys.getenv("GRIDSTATUS_API_KEY")){
+get_api_request <- function(query_url, api_key = Sys.getenv("GRIDSTATUS_API_KEY"),
+                            parse_request = TRUE){
 
   # send GET request
   resp <- httr::GET(query_url, httr::add_headers('x-api-key' = api_key), httr::user_agent("rgridstatus (https://github.com/andypicke/rgridstatus)"))
@@ -14,9 +16,19 @@ get_api_request <- function(query_url, api_key = Sys.getenv("GRIDSTATUS_API_KEY"
     stop(paste("API returned not 200 status code: ", resp$status_code))
   }
 
-  #parse response
-  resp_parsed <- jsonlite::fromJSON(httr::content(resp, as = "text", encoding = "UTF-8"))
+  if(parse_request == TRUE) {
+    #parse response
+    resp_parsed <- jsonlite::fromJSON(httr::content(resp, as = "text", encoding = "UTF-8"))
 
-  # return the data
-  df <- resp_parsed$data
+    # return the data
+    df <- resp_parsed$data
+  }
+
+  if(parse_request == FALSE) {
+    # return raw json
+    df <- resp
+  }
+
+  return(df)
+
 }
